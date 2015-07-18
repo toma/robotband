60d45f6328826029966beb4a0b373dde2772eadd
Tom Allen <tom.allen@intentmedia.com>
2015-07-17 20:49:44 -0400

    TA: Read commands from serial, either STOP or song name

--- conductor/PianoRoll.h
+++ conductor/PianoRoll.h
@@ -3,12 +3,21 @@
 // An instruction set is the ordered series of state sets and corresponding
 //     delays for the entire performance
 
+#ifndef PIANOROLL_H
+#define PIANOROLL_H
+
+#include "Arduino.h"
+
 class PianoRoll {
   public: 
     void initSD();
+    void loadSong(String songName);
     int getDelay();
     int getMusicianType();
     unsigned char* getStateSet();
     void readLine();
     bool hasRows();
+    void stop();
 };
+
+#endif
--- conductor/conductor.ino
+++ conductor/conductor.ino
@@ -8,6 +8,9 @@ PianoRoll pianoRoll;
 Musician* musician;
 unsigned char* stateSet;
 
+String serialAction = "";
+String inputString = "";
+
 void setup() 
 {
   Serial.begin(9600); // For debugging
@@ -23,22 +26,32 @@ void setup()
   pianoRoll.initSD();
 
 } 
- 
+
 void loop()
 { 
-  if (pianoRoll.hasRows()) {
-    pianoRoll.readLine();
-    stateSet = pianoRoll.getStateSet();
-    int loopDelay = pianoRoll.getDelay();
-  
-    Serial.print("Delay: ");
-    Serial.println(loopDelay);
-    Serial.print("Right Arm: ");
-    Serial.println(stateSet[0]);
-    Serial.print("Left Arm: ");
-    Serial.println(stateSet[1]);
-    musician->setState(stateSet[0], stateSet[1]);
-    
-    delay(pianoRoll.getDelay());
-  }
+    if (Serial.available() > 0) {
+        inputString = Serial.readStringUntil('\n');
+        Serial.print("READ STRING: ");
+        Serial.println(inputString);
+        if (inputString == "STOP") {
+            pianoRoll.stop();
+    	} else {
+       	    pianoRoll.loadSong(inputString);
+        }
+    } 
+//	if (pianoRoll.hasRows()) {
+		pianoRoll.readLine();
+		stateSet = pianoRoll.getStateSet();
+		int loopDelay = pianoRoll.getDelay();
+
+		Serial.print("Delay: ");
+		Serial.println(loopDelay);
+		Serial.print("Right Arm: ");
+		Serial.println(stateSet[0]);
+		Serial.print("Left Arm: ");
+		Serial.println(stateSet[1]);
+		musician->setState(stateSet[0], stateSet[1]);
+
+		delay(pianoRoll.getDelay());
+//	}
 }
